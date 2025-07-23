ced03f921433577540a8010a0b9df4cf88b2972a
Memonul Islam Tuhin <memonulislam@gmail.com>
2025-07-23 08:45:55 +0600

    Update practice_problem_4.cpp

--- practice_problem_4.cpp
+++ practice_problem_4.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 int main()
 {
-    queue<int> n;
+    int N;
+    cin >> N;
+
+    queue<int> originalQueue;
+    stack<int> tempStack;
+    for (int i = 0; i < N; ++i)
+    {
+        int value;
+        cin >> value;
+        originalQueue.push(value);
+    }
+
+    while (!originalQueue.empty())
+    {
+        tempStack.push(originalQueue.front());
+        originalQueue.pop();
+    }
+
+    queue<int> reversedQueue;
+    while (!tempStack.empty())
+    {
+        reversedQueue.push(tempStack.top());
+        tempStack.pop();
+    }
+
+    while (!reversedQueue.empty())
+    {
+        cout << reversedQueue.front() << " ";
+        reversedQueue.pop();
+    }
 
     return 0;
 }
