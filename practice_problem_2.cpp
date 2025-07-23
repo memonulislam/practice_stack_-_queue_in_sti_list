7f9a665a907f6f2f621d82c4216a57cc862bb8e9
Memonul Islam Tuhin <memonulislam@gmail.com>
2025-07-23 08:51:55 +0600

    Update practice_problem_2.cpp

--- practice_problem_2.cpp
+++ practice_problem_2.cpp
@@ -3,8 +3,50 @@ using namespace std;
 
 int main()
 {
-    stack<int> n;
-    queue<int> m;
+    int n, m, val;
+    stack<int> st;
+    queue<int> q;
+
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> val;
+        st.push(val);
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> val;
+        q.push(val);
+    }
+
+    if (n != m)
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+
+    bool isSame = true;
+    while (!st.empty() && !q.empty())
+    {
+        if (st.top() != q.front())
+        {
+            isSame = false;
+            break;
+        }
+        st.pop();
+        q.pop();
+    }
+
+    if (isSame)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
 
     return 0;
 }
